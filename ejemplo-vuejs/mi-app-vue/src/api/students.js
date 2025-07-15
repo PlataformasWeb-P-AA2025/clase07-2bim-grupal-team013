3742e699a90700fe7ac236ba19a14e32a1cd9541
JoseDavid-gif <116086797+JoseDavid-gif@users.noreply.github.com>
2025-07-15 09:55:27 -0500

    Create students.js

---
+++ ejemplo-vuejs/mi-app-vue/src/api/students.js
@@ -0,0 +1,29 @@
+import api from "./axios"; // Importa la instancia de axios que ya configuraste
+
+export default {
+  // Función para obtener todos los estudiantes (si no la tienes en otro lado)
+  getEstudiantes() {
+    return api.get("estudiantes/"); // Asume que la URL para listar es /api/estudiantes/
+  },
+
+  
+  getEstudianteByUrl(estudianteUrl) {
+    
+    return api.get(estudianteUrl);
+  },
+
+  
+  createEstudiante(estudianteData) {
+    return api.post("estudiantes/", estudianteData);
+  },
+
+  
+  updateEstudiante(estudianteUrl, estudianteData) {
+    return api.put(estudianteUrl, estudianteData); // O .patch() si tu API permite actualizaciones parciales
+  },
+
+  
+  deleteEstudiante(estudianteUrl) {
+    return api.delete(estudianteUrl);
+  },
+};
